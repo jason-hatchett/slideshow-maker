59c36a4e46ea1ce53be3fabf5fd31d250a5b2abc
tmctigrit <mctigrit.tricia@gmail.com>
2015-02-06 13:19:02 -0600

    New stuff.

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,17 +1,4 @@
 
-
-$(window).scroll(function() {
-  console.log("Nando!");
-  if (window.scrollY >= 406) {
-    $(".parallex1").css("right", "600px");
-    $(".parallex2").css("right", "450px");
-    $(".parallex3").css("right", "300px");
-    $(".parallex4").css("right", "150px");
-    $(".parallex5").css("right", "20px");
-    }
-});
-
-
 $(window).bind("scroll", function() {
   if (window.scrollY > 60) {
         $(".cloud").fadeOut(1500);
